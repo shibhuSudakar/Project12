4a3a72a2808f721c734fedfe47e39bb80eaca5bb
shibhuSudakar <antony.s1225@gmail.com>
2024-04-14 13:45:38 -0400

    remove the 2 printf statements

--- FileName.cpp
+++ FileName.cpp
@@ -5,12 +5,6 @@ int main() {
     int num1, num2, sum;
 
     
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
 
     
     sum = num1 + num2;
